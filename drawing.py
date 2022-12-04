a32e54b6d7585d75a7100ed22d7f307638041ed2
icedemon72 <knife747@gmail.com>
2022-12-05 00:58:15 +0100

    Update drawing.py

--- drawing.py
+++ drawing.py
@@ -47,6 +47,5 @@ def draw_b(turtle_object, y_start_coordinate = 0, x_start_coordinate = 0, letter
   turtle_object.pendown()
 
   turtle_object.setpos(x_start_coordinate + (letter_width / 2), y_start_coordinate)
-  turtle_object.circle((letter_width / 2), 180)
-  turtle_object.hideturtle()
+  turtle_object.circle((letter_width / 2), 181)
   turtle_object.hideturtle()
